6d5af64d5566bf4133c65096fb1cb95c376adf8f
anasiaqi <52641253+anasiaqi@users.noreply.github.com>
2020-08-26 02:21:42 -0700

    Revert "Revert "Merge branch 'master' into Minxue""
    This reverts commit 6c184b459e6632f9c770356fa8946598d4e13582.

---
+++ Assets/Scenes/_Interior_New/Pooled/MakeBall.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBall : MonoBehaviour {
+	//[Header("The ball was already present in the object pooler")]
+	public int ballIndex = 0;
+    public float spawnInterval = 0.1f;
+    float spawnProgress;
+    private int ballzero;
+    private float scales;
+    public float scale = 2f;
+	//[Header("This ball will be added to the object pooler when the game begins")]
+	//public GameObject differentBall;
+	//private int differentIndex;
+	private ObjectPooler OP;
+	public Transform SpawnPoint;
+	private void Start()
+	{
+		OP = ObjectPooler.SharedInstance;
+		//differentIndex = OP.AddObject(differentBall);
+		Random.InitState((int)System.DateTime.Now.Ticks);
+        InvokeRepeating("Generate", 1.0f, spawnInterval);
+    }
+	// Update is called once per frame
+
+
+   void Generate()
+    {
+        ballzero = Random.Range(0, ballIndex);
+        scales = Random.Range(0.2f, scale);
+        GameObject ball = OP.GetPooledObject(ballzero);
+        //ball.transform.rotation = SpawnPoint.transform.rotation;
+        float xPos = Random.Range(-10f, 10f);
+        float zPos = Random.Range(-10f, 10f);
+        float yPos = Random.Range(-10f, 10f);
+        ball.transform.position = SpawnPoint.transform.position + xPos * Vector3.right + zPos * Vector3.forward + yPos * Vector3.left;
+        ball.transform.rotation = Random.rotation;
+        ball.transform.localScale = scales * ball.transform.localScale;
+        ball.SetActive(true);
+        
+    }
+
+   
+}
