a7e7d1c076e961d8be4df3fd75f8005c7a2161bf
Trible-u <wujiexun@hotmail.com>
2020-09-04 03:54:37 -0700

    update neighborhood

---
+++ Assets/Scenes/_Neighborhood/Remember.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Remember : MonoBehaviour
+{
+    public GameObject show;
+    void Start()
+    {
+        show.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            show.gameObject.SetActive(true);
+    }
+
+    
+}
